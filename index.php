4ccd378257723b6aff65a6c8e91a9e2254565d97
cedbiyo <cedbiyo@gmail.com>
2012-12-18 19:07:15 +0100

    Ajout des liens

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>
+			Page de simulation
+		</title>
+		<script type="text/javascript" src="/dclivechat/dclivechat_tracker.js"></script>
+	</head>
+	<body>
+	
+	</body>
+</html>
